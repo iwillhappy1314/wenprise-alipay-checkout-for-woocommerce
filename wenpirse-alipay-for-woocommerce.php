56e19f620684e90025737e7ef7926472a736a485
一刀 <iwillhappy1314@gmail.com>
2019-01-14 15:26:36 +0800

    更新 'wenpirse-alipay-for-woocommerce.php'

--- wenpirse-alipay-for-woocommerce.php
+++ wenpirse-alipay-for-woocommerce.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Wenprise Alipay Checkout For WooCommerce
- * Plugin URI: https://www.wpzhiku.com
+ * Plugin URI: https://www.wpzhiku.com/wenprise-alipay-for-woocommerce
  * Description: Alipay Checkout For WooCommerce
  * Version: 1.0.1
  * Author: WenPrise Co., Ltd

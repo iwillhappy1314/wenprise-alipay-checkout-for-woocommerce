e3fb8d36e0c7e2811055b315ad0052877d552fc1
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2019-09-24 08:54:45 +0800

    scripts fix

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,66 +1,68 @@
 (function ($) {
-  var loop_count = 50
-  var loop_time = 1000
-  var wprs_woo_alipay_query_order
-  var confirm_modal = $('#js-alipay-confirm-modal')
+    var loop_count = 50;
+    var loop_time = 1000;
+    var wprs_woo_alipay_query_order;
+    var confirm_modal = $('#js-alipay-confirm-modal');
 
-  /**
-   * 点击提交支付表单
-   */
-  $('#alipay-submit-button').click()
+    /**
+     * 点击提交支付表单
+     */
+    $('#alipay-submit-button').click();
 
-  if (confirm_modal.length !== 0) {
-    $.blockUI({
-      message: confirm_modal,
-      css    : {
-        width : '500px',
-        height: '290px',
-      },
-    })
-  }
-
-  /**
-   * 查询订单支付结果
-   * @param manual 是否手动检查，手动检查时，无论拍支付是否成功，均需要跳转页面
-   */
-  wprs_woo_alipay_query_order = function (manual = false) {
-
-    if (confirm_modal.length === 0) {
-      return false
+    if (confirm_modal.length !== 0) {
+        $.blockUI({
+            message: confirm_modal,
+            css: {
+                width: '500px',
+                height: '290px',
+            },
+        });
     }
 
-    $.ajax({
-      type   : 'POST',
-      url    : WpWooAlipayData.query_url,
-      data   : {
-        order_id: $('input[name=order_id]').val(),
-      },
-      success: function (data) {
-        if (data && data.success === true || manual === true) {
-          location.href = data.data
-        } else {
-          if (loop_count-- > 0) {
-            setTimeout(wprs_woo_alipay_query_order, loop_time)
-          }
+    /**
+     * 查询订单支付结果
+     * @param manual 是否手动检查，手动检查时，无论拍支付是否成功，均需要跳转页面
+     */
+    wprs_woo_alipay_query_order = function (manual) {
+
+        var manual_trigger = arguments[0] ? arguments[0] : false;
+
+        if (confirm_modal.length === 0) {
+            return false;
         }
-      },
-      error  : function (data) {
-        if (loop_count-- > 0) {
-          setTimeout(wprs_woo_alipay_query_order, loop_time)
-        }
-      },
-    })
-  }
 
-  /**
-   * 支付成功后，如果没有自动跳转，点击按钮查询订单并跳转支付结果
-   */
-  $('#js-alipay-success, #js-alipay-fail').click(function () {
-    $.blockUI({ message: '<div style="padding: 1rem;">订单查询中...</div>' })
+        $.ajax({
+            type: 'POST',
+            url: WpWooAlipayData.query_url,
+            data: {
+                order_id: $('input[name=order_id]').val(),
+            },
+            success: function (data) {
+                if (data && data.success === true || manual_trigger === true) {
+                    location.href = data.data;
+                } else {
+                    if (loop_count-- > 0) {
+                        setTimeout(wprs_woo_alipay_query_order, loop_time);
+                    }
+                }
+            },
+            error: function (data) {
+                if (loop_count-- > 0) {
+                    setTimeout(wprs_woo_alipay_query_order, loop_time);
+                }
+            },
+        });
+    };
 
-    wprs_woo_alipay_query_order(true)
-  })
+    /**
+     * 支付成功后，如果没有自动跳转，点击按钮查询订单并跳转支付结果
+     */
+    $('#js-alipay-success, #js-alipay-fail').click(function () {
+        $.blockUI({message: '<div style="padding: 1rem;">订单查询中...</div>'});
 
-  wprs_woo_alipay_query_order()
+        wprs_woo_alipay_query_order(true);
+    });
 
-})(jQuery)
+    wprs_woo_alipay_query_order();
+
+})(jQuery);

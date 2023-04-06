5e775cdb49e217a52dfadb4d34282209445553c2
Amos Lee <iwillhappy1314@gmail.com>
2023-04-06 12:36:15 +0800

    金额精确度遵守WooCommerce设置

--- class-checkout.php
+++ class-checkout.php
@@ -422,7 +422,7 @@ class Wenprise_Alipay_Gateway extends \WC_Payment_Gateway
             $exchange_rate = 1;
         }
 
-        $total = round($total * $exchange_rate, 2);
+        $total = round($total * $exchange_rate, get_option('woocommerce_price_num_decimals'));
 
         do_action('wenprise_woocommerce_alipay_before_process_payment');
 
@@ -742,8 +742,8 @@ class Wenprise_Alipay_Gateway extends \WC_Payment_Gateway
             $exchange_rate = 1;
         }
 
-        $total         = round($total * $exchange_rate, 2);
-        $refund_amount = round($amount * $exchange_rate, 2);
+        $total         = round($total * $exchange_rate, get_option('woocommerce_price_num_decimals'));
+        $refund_amount = round($amount * $exchange_rate, get_option('woocommerce_price_num_decimals'));
 
         if ($refund_amount <= 0 || $refund_amount > $total) {
             return false;
